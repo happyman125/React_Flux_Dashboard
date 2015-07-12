bea474eefede817bf23ae8373af9a74cc7bfe44c
happyman125 <boffice933@gmail.com>
2015-07-13 05:54:28 +0800

    Updated weather alert display and styles

--- js/components/WeatherAlerts.react.js
+++ js/components/WeatherAlerts.react.js
@@ -19,12 +19,28 @@ var WeatherAlerts = React.createClass({
     var expires = alert.expires;
     var formattedexpires = Moment(expires * 1000).fromNow();
     var description = alert.description || "";
-    description = description.substring(0, 400) + "...";
+    description = description.substring(0, 150) + "...";
+
+    // If there is an 'elipsis' at the beginning 
+    // of the description, remove it:
+    if(description.substring(0, 3) == "...")
+    {
+      description = description.substring(3);
+    }
+
+    //  If there is an 'elipsis' in the description, 
+    //  break the description on that   
+    if(description.indexOf("...") > 0)
+    {
+      var breakLocation = description.indexOf("...");
+      description = description.substring(0, breakLocation);      
+    }
 
   	return (
 
-        <div id="forecast-alert" className="alert alert-info">
-          <b>{title}</b> expires {formattedexpires}
+        <div className="alert alert-info">
+          <span className="forecast-alert-title"><b>{title}</b> expires {formattedexpires}</span><br/>
+          <span>{description}</span>
         </div>
     );
   }
