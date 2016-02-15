06c505f13dd2a409a78273c23eb9058ff5279f95
happyman125 <boffice933@gmail.com>
2016-02-15 13:38:10 +0800

    Creating build.json as part of staging

--- stagingscript.sh
+++ stagingscript.sh
@@ -1,3 +1,12 @@
+# Create the build.json
+cat <<EOT > build.json
+{
+  "version" : "$CIRCLE_BUILD_NUM",
+  "project" : "$CIRCLE_PROJECT_REPONAME"
+}
+EOT
+
+
 # Copy selected files to staging area
 cp -r css $CIRCLE_ARTIFACTS
 cp -r font $CIRCLE_ARTIFACTS
