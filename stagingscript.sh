0d25d6efcc6b53b0b8cf9d0287be4ff56c93a676
happyman125 <boffice933@gmail.com>
2016-02-16 05:50:34 +0800

    Tweaking post build script

--- stagingscript.sh
+++ stagingscript.sh
@@ -3,11 +3,14 @@ rm -rf /root/dashboard.cagedtornado.com
 mkdir /root/dashboard.cagedtornado.com
 mkdir /root/dashboard.cagedtornado.com/js
 
+# See what is available
+tree -if --noreport /root/dashboard
+
 # Copy selected files to staging area
 cp -r /root/dashboard/css /root/dashboard.cagedtornado.com
 cp -r /root/dashboard/font /root/dashboard.cagedtornado.com
 cp -r /root/dashboard/fonts /root/dashboard.cagedtornado.com
-cp /root/dashboard/js/*.js /root/dashboard.cagedtornado.com/js/
+cp /root/dashboard/js/bundle.js /root/dashboard.cagedtornado.com/js/
+cp /root/dashboard/js/rainbowvis.js /root/dashboard.cagedtornado.com/js/
 cp /root/dashboard/index.html /root/dashboard.cagedtornado.com/
 cp /root/dashboard/build.json /root/dashboard.cagedtornado.com/
-cp /root/dashboard/*.js /root/dashboard.cagedtornado.com/
