94da7e7a6dd367ec4555d682e1c43496984114ec
happyman125 <boffice933@gmail.com>
2016-02-15 13:47:49 +0800

    Updated staging script

--- stagingscript.sh
+++ stagingscript.sh
@@ -13,6 +13,7 @@ mkdir $CIRCLE_ARTIFACTS/js/
 cp -r css $CIRCLE_ARTIFACTS
 cp -r font $CIRCLE_ARTIFACTS
 cp -r fonts $CIRCLE_ARTIFACTS
-cp js/*.js $CIRCLE_ARTIFACTS/js/
+cp js/bundle.js $CIRCLE_ARTIFACTS/js/
+cp js/rainbowvis.js $CIRCLE_ARTIFACTS/js/
 cp index.html $CIRCLE_ARTIFACTS
 cp build.json $CIRCLE_ARTIFACTS
