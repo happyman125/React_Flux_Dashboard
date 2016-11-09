ca95760eef61f49514c22c3d8a69380d6ff95cd5
happyman125 <boffice933@gmail.com>
2016-11-10 03:30:01 +0800

    Script removal
    Don't need these scripts anymore to deploy.

--- deployscript.sh
+++
@@ -1,3 +0,0 @@
-# This script is run when the docker container starts
-# It expects the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to be set
-s3cmd sync /root/dashboard.cagedtornado.com/ s3://dashboard.cagedtornado.com --acl-public --no-mime-magic --guess-mime-type
